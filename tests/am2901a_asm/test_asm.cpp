e41a8a98451d8691ae24db22ee3b75ab8febd1f5
htmlgod <ivannevtrilov212@gmail.com>
2021-03-29 06:12:50 +0300

    project: testing...

--- tests/am2901a_asm/test_asm.cpp
+++ tests/am2901a_asm/test_asm.cpp
@@ -2,13 +2,37 @@
 
 #include<gtest/gtest.h>
 
-TEST(PARSER_TEST, BASIC) {
-    AM2901A_ASM::AM2901A_ASM ASM {};
-    std::string line = "ADD AQ,QREG,1,2,3";
-    AM2901A_ASM::COMMAND res = ASM.parseCommand(line);
-    AM2901A_ASM::COMMAND cmd = {"ADD","AQ",1,2,3,"QREG"};
-    bool eq = res.A == cmd.A and res.B == cmd.B and res.D == cmd.D and
-              res.destination == cmd.destination and res.operands == cmd.operands and res.operation == cmd.operation;
+class AM2901A_ASM_TEST : public ::testing::Test {
+protected:
+    AM2901A_ASM::AM2901A_ASM ASM{};
+    void SetUp() override {
+       
+    }
 
-    EXPECT_EQ(eq, true);
+    struct flagsCheck {
+        BYTE OVR : 1;
+        BYTE C4 : 1;
+        BYTE F3 : 1;
+        BYTE Z : 1;
+        BYTE G : 1;
+        BYTE P : 1;
+        BYTE Y : 4;
+    } flagsCheck;
+};
+
+TEST_F(AM2901A_ASM_TEST, comment_parse_1) {
+    std::string line = "#comment";
+    ASSERT_TRUE(AM2901A_ASM::AM2901A_ASM::isCommentary(line));
+
+}
+TEST_F(AM2901A_ASM_TEST, comment_parse_2) {
+    std::string line = "# ## # # # comment";
+    ASSERT_TRUE(AM2901A_ASM::AM2901A_ASM::isCommentary(line));
+}
+TEST_F(AM2901A_ASM_TEST, comment_parse_3) {
+    std::string line = "#    123123 AD        FASda gsdDASdas     QE2313comment";
+    ASSERT_TRUE(AM2901A_ASM::AM2901A_ASM::isCommentary(line));
+}
+TEST_F(AM2901A_ASM_TEST, work_with_registers) {
+    EXPECT_ANY_THROW(ASM.parseCommand("#adasd"));
 }
